3e4718d80374a341dd679dccd6f4be674a47f8e3
despitiab <95830595+despitiab@users.noreply.github.com>
2021-12-11 16:10:44 -0500

    scroll

--- js/custom.js
+++ js/custom.js
@@ -8,34 +8,31 @@ $(document).ready(function(){
         
         'use strict';
 
-        
-        var heightPage = $(document).height();
-
-            if(($(window).scrollTop() + $(window).height()) == heightPage) {
-                $('.navbar').css ({
-                    'margin-top': '0px',
-                     'opacity': '1'
-                     
-                 });
-
-                 $('.navbar-default').css({
-                    'background-color': 'rgba(59, 59 , 59, 0.7)',
-                    'border-color': '#444'
-                    
-                });
-                
-                $('.navbar-brand img').css({
-                   'height': '35px',
-                    'padding-top': '0px'
-                    
-                });
-                
-                $('.navbar-nav > li > a ').css({
-                    'padding-top': '15px'
-                    
-                    
-                });
-            }      
+        if($(window).scrollTop() === 0 ) {
+            $('.navbar').css ({
+                'margin-top': '0px',
+                 'opacity': '1'
+                 
+             });
+             
+             $('.navbar-default').css({
+                 'background-color': 'rgba(59, 59 , 59, 0.7)',
+                 'border-color': '#444'
+                 
+             });
+             
+             $('.navbar-brand img').css({
+                'height': '35px',
+                 'padding-top': '0px'
+                 
+             });
+             
+             $('.navbar-nav > li > a ').css({
+                 'padding-top': '15px'
+                 
+                 
+             });
+        }
         
         if($(window).scrollTop() < 80 ) {
             
