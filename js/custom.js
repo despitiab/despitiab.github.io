246c79df53ee6f43d336737996234ff0a27de531
despitiab <95830595+despitiab@users.noreply.github.com>
2021-12-11 16:21:11 -0500

    scroll

--- js/custom.js
+++ js/custom.js
@@ -7,34 +7,8 @@ $(document).ready(function(){
     $(window).scroll(function() {
         
         'use strict';
-
-        if($(window).scrollTop() == 0 ) {
-            $('.navbar').css ({
-                'margin-top': '0px',
-                 'opacity': '1'
-                 
-             });
-             
-             $('.navbar-default').css({
-                 'background-color': 'rgba(59, 59 , 59, 0.7)',
-                 'border-color': '#444'
-                 
-             });
-             
-             $('.navbar-brand img').css({
-                'height': '35px',
-                 'padding-top': '0px'
-                 
-             });
-             
-             $('.navbar-nav > li > a ').css({
-                 'padding-top': '15px'
-                 
-                 
-             });
-        }
         
-        if($(window).scrollTop() < 80 ) {
+        if($(window).scrollTop() < 60 ) {
             
             $('.navbar').css ({
                'margin-top': '-100px',
